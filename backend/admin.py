041c92d9126c8ae2d7117d68a6ba321812b58bb5
LXJ0000 <202100203017@stumail.sztu.edu.cn>
2024-07-19 14:26:19 +0800

    feat: 自定义后台表单

--- backend/admin.py
+++ backend/admin.py
@@ -3,5 +3,17 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ["published_date", "question_text"]
+    list_display = ("question_text", "published_date", "was_published_recently")
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["published_date"]}),
+    ]
+    list_filter = ["published_date"]
+    search_fields = ["question_text"]
+
+
 admin.site.register(models.Choice)
-admin.site.register(models.Question)
+admin.site.register(models.Question, QuestionAdmin)
